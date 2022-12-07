d25fd40c7749863d372e7c2692a1a430a975e8a2
Andy Driver <andy@pagezero.net>
2022-12-07 21:31:13 +0000

    Refactor days 1 and 2 to use literate programming

--- 2022/day1.py
+++ 2022/day1.py
@@ -1,32 +1,156 @@
+"""
+--- Day 1: Calorie Counting ---
+
+Santa's reindeer typically eat regular reindeer food, but they need a lot of
+magical energy to deliver presents on Christmas. For that, their favorite snack
+is a special type of star fruit that only grows deep in the jungle. The Elves
+have brought you on their annual expedition to the grove where the fruit grows.
+
+To supply enough magical energy, the expedition needs to retrieve a minimum of
+fifty stars by December 25th. Although the Elves assure you that the grove has
+plenty of fruit, you decide to grab any fruit you see along the way, just in
+case.
+
+Collect stars by solving puzzles. Two puzzles will be made available on each day
+in the Advent calendar; the second puzzle is unlocked when you complete the
+first. Each puzzle grants one star. Good luck!
+
+The jungle must be too overgrown and difficult to navigate in vehicles or access
+from the air; the Elves' expedition traditionally goes on foot. As your boats
+approach land, the Elves begin taking inventory of their supplies. One important
+consideration is food - in particular, the number of Calories each Elf is
+carrying (your puzzle input).
+
+The Elves take turns writing down the number of Calories contained by the
+various meals, snacks, rations, etc. that they've brought with them, one item
+per line. Each Elf separates their own inventory from the previous Elf's
+inventory (if any) by a blank line.
+
+For example, suppose the Elves finish writing their items' Calories and end
+up with the following list:
+"""
+
+example_input = """
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+"""
+
+example_food_items = example_input.strip().splitlines()
+
 from itertools import groupby, islice
 
 
-def by_elf(data):
-    data = data.strip().splitlines()
-    for key, group in groupby(data, lambda s: bool(s.strip())):
-        if key:
-            yield map(int, group)
+def grouped_by_elf(food_items):
+    """
+    This list represents the Calories of the food carried by five Elves:
+
+    - The first Elf is carrying food with 1000, 2000, and 3000 Calories, a total
+      of 6000 Calories.
+
+      >>> list(grouped_by_elf(example_food_items))[0]
+      [1000, 2000, 3000]
+
+      >>> sum(_)
+      6000
+
+    - The second Elf is carrying one food item with 4000 Calories.
+
+      >>> list(grouped_by_elf(example_food_items))[1]
+      [4000]
+
+    - The third Elf is carrying food with 5000 and 6000 Calories, a total of
+      11000 Calories.
+
+      >>> list(grouped_by_elf(example_food_items))[2]
+      [5000, 6000]
+
+      >>> sum(_)
+      11000
+
+    - The fourth Elf is carrying food with 7000, 8000, and 9000 Calories, a
+      total of 24000 Calories.
+
+      >>> list(grouped_by_elf(example_food_items))[3]
+      [7000, 8000, 9000]
+
+      >>> sum(_)
+      24000
+
+    - The fifth Elf is carrying one food item with 10000 Calories.
+
+      >>> list(grouped_by_elf(example_food_items))[4]
+      [10000]
+    """
+    for not_empty, group in groupby(food_items, bool):
+        if not_empty:
+            yield list(map(int, group))
 
 
-def sorted_calorie_subtotals(food_items_by_elf):
+def sorted_calorie_totals(food_items_by_elf):
+    """
+    In case the Elves get hungry and need extra snacks, they need to know which
+    Elf to ask: they'd like to know how many Calories are being carried by the
+    Elf carrying the most Calories. In the example above, this is 24000 (carried
+    by the fourth Elf).
+
+    >>> list(sorted_calorie_totals(grouped_by_elf(example_food_items)))[0]
+    24000
+    """
     yield from reversed(sorted(map(sum, food_items_by_elf)))
 
 
-def part_1(data):
-    return sum(islice(sorted_calorie_subtotals(by_elf(data)), 1))
+food_items = open("day1-input.txt").read().strip().splitlines()
 
 
-def part_2(data):
-    return sum(islice(sorted_calorie_subtotals(by_elf(data)), 3))
+def part1_answer():
+    """
+    Find the Elf carrying the most Calories. How many total Calories is that Elf
+    carrying?
+    """
+    return sum(islice(sorted_calorie_totals(grouped_by_elf(food_items)), 1))
 
 
-def main():
-    with open("day1-input.txt") as f:
-        data = f.read()
+"""
 
-    print(f"part_1: most calories carried {part_1(data)}")
-    print(f"part_2: top three elves calories {part_2(data)}")
+## --- Part Two ---
+
+By the time you calculate the answer to the Elves' question, they've already
+realized that the Elf carrying the most Calories of food might eventually run
+out of snacks.
+
+To avoid this unacceptable situation, the Elves would instead like to know the
+total Calories carried by the top three Elves carrying the most Calories. That
+way, even if one of those Elves runs out of snacks, they still have two backups.
+
+In the example above, the top three Elves are the fourth Elf (with 24000
+Calories), then the third Elf (with 11000 Calories), then the fifth Elf (with
+10000 Calories). The sum of the Calories carried by these three elves is 45000.
+"""
+
+
+def part2_answer(food_items):
+    """
+    Find the top three Elves carrying the most Calories. How many Calories are
+    those Elves carrying in total?
+
+    >>> part2_answer(example_food_items)
+    45000
+    """
+    return sum(islice(sorted_calorie_totals(grouped_by_elf(food_items)), 3))
 
 
 if __name__ == "__main__":
-    main()
+    print(f"part 1: {part1_answer()}")
+    print(f"part 2: {part2_answer(food_items)}")
--- 2022/day2.py
+++ 2022/day2.py
@@ -1,45 +1,160 @@
-def rounds(input):
-    yield from map(lambda _: _.split(" "), input.strip().splitlines())
+"""
+--- Day 2: Rock Paper Scissors ---
+
+The Elves begin to set up camp on the beach. To decide whose tent gets to be
+closest to the snack storage, a giant Rock Paper Scissors tournament is already
+in progress.
+
+Rock Paper Scissors is a game between two players. Each game contains many
+rounds; in each round, the players each simultaneously choose one of Rock,
+Paper, or Scissors using a hand shape. Then, a winner for that round is
+selected: Rock defeats Scissors, Scissors defeats Paper, and Paper defeats Rock.
+If both players choose the same shape, the round instead ends in a draw.
+
+Appreciative of your help yesterday, one Elf gives you an encrypted strategy
+guide (your puzzle input) that they say will be sure to help you win. "The first
+column is what your opponent is going to play: A for Rock, B for Paper, and C
+for Scissors. The second column--" Suddenly, the Elf is called away to help with
+someone's tent.
+
+The second column, you reason, must be what you should play in response: X for
+Rock, Y for Paper, and Z for Scissors. Winning every time would be suspicious,
+so the responses must have been carefully chosen.
+
+The winner of the whole tournament is the player with the highest score. Your
+total score is the sum of your scores for each round. The score for a single
+round is the score for the shape you selected (1 for Rock, 2 for Paper, and 3
+for Scissors) plus the score for the outcome of the round (0 if you lost, 3 if
+the round was a draw, and 6 if you won).
+
+Since you can't be sure if the Elf is trying to help you or trick you, you
+should calculate the score you would get if you were to follow the strategy
+guide.
+
+For example, suppose you were given the following strategy guide:
+"""
+
+example_input = """
+A Y
+B X
+C Z
+"""
+
+example_rounds = example_input.strip().splitlines()
+
+outcomes = {
+    #     R  P  S
+    "A": [3, 6, 0],  # R
+    "B": [0, 3, 6],  # P
+    "C": [6, 0, 3],  # S
+}
 
 
-def part_1(input):
-    outcomes = {
-        #     R  P  S
-        "A": [3, 6, 0],  # R
-        "B": [0, 3, 6],  # P
-        "C": [6, 0, 3],  # S
-    }
+def score(round):
+    """
+    This strategy guide predicts and recommends the following:
 
-    score = 0
-    for opp, you in rounds(input):
-        your_shape = "XYZ".index(you)
-        score += 1 + your_shape
-        score += outcomes[opp][your_shape]
-    return score
+    - In the first round, your opponent will choose Rock (A), and you should
+      choose Paper (Y). This ends in a win for you with a score of 8 (2 because
+      you chose Paper + 6 because you won).
+
+      >>> score(example_rounds[0])
+      8
+
+    - In the second round, your opponent will choose Paper (B), and you should
+      choose Rock (X). This ends in a loss for you with a score of 1 (1 + 0).
+
+      >>> score(example_rounds[1])
+      1
+
+    - The third round is a draw with both players choosing Scissors, giving you
+      a score of 3 + 3 = 6.
+
+      >>> score(example_rounds[2])
+      6
+
+    In this example, if you were to follow the strategy guide, you would get a total
+    score of 15 (8 + 1 + 6).
+
+    >>> sum(map(score, example_rounds))
+    15
+    """
+    opp, you = round.split(" ")
+    your_shape = "XYZ".index(you)
+    return (1 + your_shape) + outcomes[opp][your_shape]
 
 
-def part_2(input):
-    plays = {
-        #     L  D  W
-        "A": [2, 0, 1],  # R
-        "B": [0, 1, 2],  # P
-        "C": [1, 2, 0],  # S
-    }
-
-    score = 0
-    for opp, outcome in rounds(input):
-        outcome_idx = "XYZ".index(outcome)
-        score += 1 + plays[opp][outcome_idx]
-        score += outcome_idx * 3
-    return score
+rounds = open("day2-input.txt").read().strip().splitlines()
 
 
-def main():
-    with open("day2-input.txt") as f:
-        input = f.read()
-    print(f"part_1: total score = {part_1(input)}")
-    print(f"part_2: total score = {part_2(input)}")
+def part1_answer():
+    """
+    What would your total score be if everything goes exactly according to your
+    strategy guide?
+    """
+    return sum(map(score, rounds))
+
+
+"""
+## --- Part Two ---
+
+The Elf finishes helping with the tent and sneaks back over to you. "Anyway, the
+second column says how the round needs to end: X means you need to lose, Y means
+you need to end the round in a draw, and Z means you need to win. Good luck!"
+"""
+
+plays = {
+    #     L  D  W
+    "A": [2, 0, 1],  # R
+    "B": [0, 1, 2],  # P
+    "C": [1, 2, 0],  # S
+}
+
+
+def score2(round):
+    """
+    The total score is still calculated in the same way, but now you need to
+    figure out what shape to choose so the round ends as indicated. The example
+    above now goes like this:
+
+    - In the first round, your opponent will choose Rock (A), and you need the
+      round to end in a draw (Y), so you also choose Rock. This gives you a
+      score of 1 + 3 = 4.
+
+      >>> score2(example_rounds[0])
+      4
+
+    - In the second round, your opponent will choose Paper (B), and you choose
+      Rock so you lose (X) with a score of 1 + 0 = 1.
+
+      >>> score2(example_rounds[1])
+      1
+
+    - In the third round, you will defeat your opponent's Scissors with Rock for
+      a score of 1 + 6 = 7.
+
+      >>> score2(example_rounds[2])
+      7
+
+    Now that you're correctly decrypting the ultra top secret strategy guide, you
+    would get a total score of 12.
+
+    >>> sum(map(score2, example_rounds))
+    12
+    """
+    opp, outcome = round.split(" ")
+    outcome_idx = "XYZ".index(outcome)
+    return (1 + plays[opp][outcome_idx]) + outcome_idx * 3
+
+
+def part2_answer():
+    """
+    Following the Elf's instructions for the second column, what would your total
+    score be if everything goes exactly according to your strategy guide?
+    """
+    return sum(map(score2, rounds))
 
 
 if __name__ == "__main__":
-    main()
+    print(f"part 1: {part1_answer()}")
+    print(f"part 2: {part2_answer()}")
